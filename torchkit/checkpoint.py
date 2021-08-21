1d4cfa7df79c5c3be5d060a8b8796fbc5c6fb4e7
Kevin Zakka <kevinzakka@users.noreply.github.com>
2021-08-21 13:08:31 -0400

    Implement cleaner checkpoint manager. (#3)
    * Implement cleaner checkpoint manager.
    
    * Remove unused type.
    
    * Make pytype happy.

--- torchkit/checkpoint.py
+++ torchkit/checkpoint.py
@@ -1,22 +1,20 @@
 import logging
 import os
-import os.path as osp
 import signal
-from glob import glob
-from typing import Callable, List, Optional
+from pathlib import Path
+from typing import Any, List, Optional, Union
 
-import numpy as np
 import torch
-from torch import nn
+
+from .experiment import unique_id
 
 
 def get_files(
-    d: str,
+    d: Path,
     pattern: str,
     sort: bool = False,
     lexicographical: bool = False,
-    sortfunc: Optional[Callable] = None,
-) -> List[str]:
+) -> List[Path]:
     """Return a list of files in a given directory.
 
     Args:
@@ -25,54 +23,25 @@ def get_files(
         sort: Whether to sort the returned list.
         lexicographical: If sort, use lexicographical order. Set to `False` for
             numerical ordering.
-        sortfunc : An optional sorting Callable to use if sort is set to `True`.
-            Ignores the value of `lexicographical`.
     """
-    if sortfunc is not None and not sort:
-        raise ValueError("`sort` must be True when `sortfunc` is provided.")
-    files = glob(osp.join(d, pattern))
-    files = [f for f in files if osp.isfile(f)]
+    files = d.glob(pattern)
+    files = [f for f in files if f.is_file()]
     if sort:
-        if sortfunc is not None:
-            files.sort(key=sortfunc)
+        if lexicographical:
+            files.sort(key=lambda x: x.name)
         else:
-            if lexicographical:
-                files.sort(key=lambda x: osp.basename(x))
-            else:
-                files.sort(key=lambda x: int(x.split("/")[-1].split(".")[0]))
+            files.sort(key=lambda x: x.stem)
     return files
 
 
 class Checkpoint:
-    """Save and restore PyTorch objects implementing `state_dict` attributes.
-
-    A `Checkpoint` object should be used in conjunction with a
-    :class:`~torchkit.checkpoint.CheckpointManager`.
-
-    Note: This is a re-implementation of `1`_.
-
-    Example usage::
-
-        import os
-        from torchkit.checkpoint import Checkpoint
-
-        checkpoint_dir = "/tmp/training_checkpoints"
-        checkpoint_path = os.path.join(checkpoint_dir, "weights.ckpt")
-
-        # Create a Checkpoint that will manage the model and optimizer.
-        checkpoint = Checkpoint(model=model, optimizer=optimizer)
-        checkpoint.save(checkpoint_path)
-        status = checkpoint.restore(checkpoint_path)
-
-    .. _1: https://www.tensorflow.org/api_docs/python/tf/train/Checkpoint
-    """
+    """Save and restore PyTorch objects implementing a `state_dict` method."""
 
     def __init__(self, **kwargs):
         """Constructor.
 
         Accepts keyword arguments whose values are objects that contain a
-        `state_dict` attribute and thus can be serialized to disk. Also accepts
-        `nn.Parameter` objects.
+        `state_dict` attribute and thus can be serialized to disk.
 
         Args:
             kwargs: Keyword arguments are set as attributes of this object,
@@ -84,14 +53,11 @@ class Checkpoint:
                 attribute.
         """
         for k, v in sorted(kwargs.items()):
-            if isinstance(v, nn.Parameter):
-                setattr(self, k, v)
-            else:
-                if not getattr(v, "state_dict"):
-                    raise ValueError(f"{k} does not have a state_dict attribute.")
-                setattr(self, k, v)
+            if not getattr(v, "state_dict"):
+                raise ValueError(f"{k} does not have a state_dict attribute.")
+            setattr(self, k, v)
 
-    def save(self, save_path: str) -> None:
+    def save(self, save_path: Path):
         """Save a state to disk.
 
         Modified from brentyi/fannypack.
@@ -107,81 +73,67 @@ class Checkpoint:
             # Signal throws a ValueError if we're not in the main thread.
             orig_handler = None
 
-        # Atomic save.
-        save_dir = osp.dirname(save_path)
-        tmp_path = osp.join(save_dir, f"tmp-{np.random.randint(1e10)}.ckpt")
+        # Create a snapshot of the current state.
         save_dict = dict()
         for k, v in self.__dict__.items():
-            if isinstance(v, nn.Parameter):
-                save_dict[k] = v
-            else:
-                save_dict[k] = v.state_dict()
-        torch.save(save_dict, tmp_path)
+            save_dict[k] = v.state_dict()
+
         # Rename is POSIX-compliant and as such, is an atomic operation
         # according to the Python docs:
         # https://docs.python.org/3/library/os.html#os.rename
+        tmp_path = save_path.parent / f"tmp-{unique_id()}.ckpt"
+        torch.save(save_dict, tmp_path)
         os.rename(tmp_path, save_path)
-        logging.info(f"Saved checkpoint at {save_path}.")
 
         # Restore SIGINT handler.
         if orig_handler is not None:
             signal.signal(signal.SIGINT, orig_handler)
 
-    def restore(
-        self,
-        save_path: str,
-        device: Optional[torch.device] = None,
-    ) -> bool:
+    def restore(self, save_path: Union[str, Path]) -> bool:
         """Restore a state from a saved checkpoint.
 
         Args:
             save_path: The filepath to the saved checkpoint.
-            device: The device on which to restore the state.
+
+        Returns:
+            True if restoring was successful or partially (not all
+            checkpointables could be restored) successful and False otherwise.
         """
         try:
-            state = torch.load(save_path, map_location=device)
-            try:
-                for name, state_dict in state.items():
-                    if not hasattr(self, name):
-                        print(
-                            f"{name} in saved checkpoint not in checkpoint to "
-                            "reload. Skipping it."
-                        )
-                        continue
-                    if isinstance(getattr(self, name), nn.Parameter):
-                        setattr(self, name, state_dict)
-                    else:
-                        try:
-                            getattr(self, name).load_state_dict(state_dict)
-                        except:  # noqa: E722
-                            print(f"Could not load {name} state.")
-                logging.info(f"Successfully loaded checkpoint state from {save_path}.")
-                return True
-            except Exception as e:
-                # There was an issue loading the state which means either the
-                # model definition and saved weights do not agree or they were
-                # not saved in the first place.
-                # Since this is a severe issue, we raise an error rather than
-                # allowing the program to proceed.
-                raise e
-        except FileNotFoundError as e:
-            logging.error(e)
+            state = torch.load(Path(save_path), map_location="cpu")
+            for name, state_dict in state.items():
+                if not hasattr(self, name):
+                    print(
+                        f"{name} in saved checkpoint not in checkpoint to "
+                        "reload. Skipping it."
+                    )
+                    continue
+                getattr(self, name).load_state_dict(state_dict)
+            return True
+        except Exception as e:
+            print(e)
             return False
 
 
+# TODO(kevin): Add saving of best checkpoint based on specified metric.
 class CheckpointManager:
-    """Manages multiple checkpoints by keeping some and deleting unneeded ones.
+    """
+    Periodically save PyTorch checkpointables (any object that implements a
+    `state_dict` method) to disk and restore them to resume training.
 
     Note: This is a re-implementation of `2`_.
 
     Example usage::
 
-        from torchkit.checkpoint import Checkpoint, CheckpointManager
+        from torchkit.checkpoint import CheckpointManager
 
         # Create a checkpoint manager instance.
         checkpoint_manager = checkpoint.CheckpointManager(
-            checkpoint.Checkpoint(model=model, optimizer=optimizer),
-            checkpoint_dir, device)
+            checkpoint_dir,
+            device,
+            model=model,
+            optimizer=optimizer,
+        )
 
         # Restore last checkpoint if it exists.
         global_step = checkpoint_manager.restore_or_initialize()
@@ -197,32 +149,27 @@ class CheckpointManager:
 
     def __init__(
         self,
-        checkpoint: Checkpoint,
         directory: str,
-        device: torch.device,
         max_to_keep: int = 10,
+        **checkpointables: Any,
     ):
         """Constructor.
 
         Args:
-            checkpoint: An instance of `Checkpoint`.
             directory: The directory in which checkpoints will be saved.
-            device: The computing device on which to restore checkpoints.
             max_to_keep: The maximum number of checkpoints to keep.
                 Amongst all saved checkpoints, checkpoints will be deleted
                 oldest first, until `max_to_keep` remain.
+            **checkpointables: Keyword args with checkpointable PyTorch objects.
         """
         assert max_to_keep > 0, "max_to_keep should be a positive integer."
 
-        self.checkpoint = checkpoint
-        self.directory = directory
+        self.directory = Path(directory).absolute()
         self.max_to_keep = max_to_keep
-        self.device = device
-        self.latest_checkpoint = None
+        self.checkpoint = Checkpoint(**checkpointables)
 
         # Create checkpoint directory if it doesn't already exist.
-        if not osp.exists(self.directory):
-            os.makedirs(self.directory)
+        self.directory.mkdir(parents=True, exist_ok=True)
 
     def restore_or_initialize(self) -> int:
         """Restore items in checkpoint from the latest checkpoint file.
@@ -232,15 +179,14 @@ class CheckpointManager:
             file if one is found, else 0 is returned.
         """
         ckpts = CheckpointManager.list_checkpoints(self.directory)
-        if ckpts:
-            last_ckpt = ckpts[-1]
-            status = self.checkpoint.restore(last_ckpt, self.device)
-            if not status:
-                logging.info("Could not restore latest checkpoint file.")
-                return 0
-            self.latest_checkpoint = last_ckpt
-            return int(osp.basename(last_ckpt).split(".")[0])
-        return 0
+        if not ckpts:
+            return 0
+        last_ckpt = ckpts[-1]
+        status = self.checkpoint.restore(last_ckpt)
+        if not status:
+            logging.info("Could not restore latest checkpoint file.")
+            return 0
+        return int(last_ckpt.stem)
 
     def save(self, global_step: int) -> None:
         """Create a new checkpoint.
@@ -249,55 +195,38 @@ class CheckpointManager:
             global_step: The iteration number which will be used to name the
                 checkpoint.
         """
-        save_path = osp.join(self.directory, "{:016d}.ckpt".format(global_step))
+        save_path = self.directory / f"{global_step}.ckpt"
         self.checkpoint.save(save_path)
-        self.latest_checkpoint = save_path
         self._trim_checkpoints()
 
     def _trim_checkpoints(self):
         """Trim older checkpoints until `max_to_keep` remain."""
-        # Get a list of checkpoints in reverse chronological order.
+        # Get a list of checkpoints in reverse global_step order.
         ckpts = CheckpointManager.list_checkpoints(self.directory)[::-1]
-
         # Remove until `max_to_keep` remain.
-        num_remove = len(ckpts) - self.max_to_keep
-        while num_remove > 0:
-            ckpt_name = ckpts.pop()
-            os.remove(ckpt_name)
-            num_remove -= 1
+        while len(ckpts) - self.max_to_keep > 0:
+            ckpts.pop().unlink()
 
-    @staticmethod
-    def load_latest_checkpoint(
-        checkpoint: Checkpoint,
-        directory: str,
-        device: torch.device,
-    ) -> None:
+    def load_latest_checkpoint(self):
         """Load the last saved checkpoint."""
-        ckpts = CheckpointManager.list_checkpoints(directory)
-        if ckpts:
-            last_ckpt = ckpts[-1]
-            checkpoint.restore(last_ckpt, device)
-        else:
-            raise ValueError(f"No checkpoints found in {directory}.")
+        self.checkpoint.restore(self.latest_checkpoint)
+
+    def load_checkpoint_at(self, global_step: int):
+        """Load a checkpoint at a given global step."""
+        ckpt_name = self.directory / f"{global_step}.ckpt"
+        if ckpt_name not in CheckpointManager.list_checkpoints(self.directory):
+            raise ValueError(f"No checkpoint found at step {global_step}.")
+        self.checkpoint.restore(ckpt_name)
+
+    @property
+    def latest_checkpoint(self) -> Optional[Path]:
+        """Get the last saved checkpoint."""
+        ckpts = CheckpointManager.list_checkpoints(self.directory)
+        if not ckpts:
+            raise ValueError(f"No checkpoints found in {self.directory}.")
+        return ckpts[-1]
 
     @staticmethod
-    def list_checkpoints(directory: str) -> List[str]:
+    def list_checkpoints(directory: Union[Path, str]) -> List[Path]:
         """List all checkpoints in a checkpoint directory."""
-        return get_files(
-            directory,
-            "*.ckpt",
-            sort=True,
-            sortfunc=lambda x: int(osp.splitext(osp.basename(x))[0]),
-        )
-
-    @staticmethod
-    def load_specific_checkpoint(
-        checkpoint: Checkpoint,
-        checkpoint_filename: str,
-        device: torch.device,
-    ) -> None:
-        """Load a specific checkpoint."""
-        try:
-            checkpoint.restore(checkpoint_filename, device)
-        except Exception as e:
-            raise e
+        return get_files(Path(directory), "*.ckpt", sort=True)
