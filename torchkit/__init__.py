d7c83289e78b047568dcd9c7aa7f41cfe6ff3d15
Kevin Zakka <kevinarmandzakka@gmail.com>
2021-08-21 18:46:36 -0400

    Allow `CheckpointManager` to be directly importable.

--- torchkit/__init__.py
+++ torchkit/__init__.py
@@ -1,4 +1,5 @@
 """A PyTorch toolkit for research."""
 
+from torchkit.checkpoint import CheckpointManager  # noqa: F401
 from torchkit.logger import Logger  # noqa: F401
 from torchkit.version import __version__  # noqa: F401
